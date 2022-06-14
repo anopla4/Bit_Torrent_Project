1ca0970c473880eb534132f02e258c14c80af6d5
abel1927 <abel.molina@estudiantes.matcom.uh.cu>
2022-06-14 09:34:59 -0400

    add: first implemention

--- tracker/communication.go
+++ tracker/communication.go
@@ -14,18 +14,40 @@ type AnnounceQuery struct {
 	Downloaded int
 	Left       int
 	Event      int
-	NumWant    int //Cantidad de peer que solicita el peer, Default 40
+	NumWant    int //Cantidad de peer que solicita el peer, Default 50
 }
+
+//IAnnounceResponse representa el tipo de una respuesta a una peticion Announce
+type IAnnounceResponse interface{}
 
 // AnnounceResponse es una representacion de una respuesta a un announce get query
 type AnnounceResponse struct {
-	FailureReason string
-	Interval      int
-	TrackerID     int
-	Complete      int16
-	Incomplete    int
-	Peers         string
+	Interval   int
+	TrackerID  int
+	Complete   int
+	Incomplete int
+	Peers      string
 }
 
-/*type ScraperQuery struct {
-}*/
+//ScraperQuery representa el contenido de una peticion Scrape al Tracker
+type ScraperQuery struct {
+	InfoHash []string
+}
+
+//File representa un file dentro de una una respuesta a peticion Scraper
+type File struct {
+	Incomplete int
+	Complete   int
+	Downloaded int
+}
+
+//ScraperResponse representa el contenido de una respuesta a peticion Scraper
+type ScraperResponse struct {
+	Files map[string]File
+}
+
+//ErrorResponse representa la respuesta cuando ocurre un error en el proceseo de algun
+//request en el Tracker
+type ErrorResponse struct {
+	FailureReason string
+}
