afc362e262a8fee9d219eadf7865aacda84d2206
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-06-18 23:46:48 -0400

    refactor: Calling client from main.go

--- Client/main.go
+++ Client/main.go
@@ -1,9 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"Bit_Torrent_Project/client/torrent_peer/downloader_client"
+	"Bit_Torrent_Project/client/torrent_peer/uploader_client"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+	"torrent_peerpb"
+)
 
 func main() {
-	var a *int
-	fmt.Printf("%T\n", &a)
-	fmt.Println(&a)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func(port string) {
+		fmt.Println("Hello World")
+
+		uploader_client.StartServer(port)
+
+		wg.Done()
+	}("50051")
+
+	go func(port string) {
+		time.Sleep(20 * time.Second)
+		cc, err := downloader_client.StartClient(port)
+
+		if err != nil {
+			log.Fatalf("Could not connect: %v", err)
+			return
+		}
+		defer cc.Close()
+		c := torrent_peerpb.NewDownloadServiceClient(cc)
+		requests := []*torrent_peerpb.Message{
+			{
+				Length: 1, Id: 1,
+			},
+			{
+				Length: 1, Id: 2,
+			},
+			{
+				Length: 1, Id: 3,
+			},
+		}
+		downloader_client.MessageStream(c, requests)
+
+		wg.Done()
+	}("6881")
+
+	wg.Wait()
 }
