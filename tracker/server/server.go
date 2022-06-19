62c20eac8a5ecbed79484d94e295424e76e5cb4e
abel1927 <abel.molina@estudiantes.matcom.uh.cu>
2022-06-18 22:58:51 -0400

    add: publish query

--- tracker/server/server.go
+++ tracker/server/server.go
@@ -6,20 +6,29 @@ import (
 	"net"
 	"time"
 
-	pb "github.com/anoppa/Bit_Torrent_Project/tracker/trackerpb"
+	pb "Bit_Torrent_Project/tracker/trackerpb"
 )
 
-// AnnounceQuery es una representacion de una announce get query
-type AnnounceQuery struct {
+// constantes para el status de un Publish Request
+const (
+	ERROR = 0
+	OK = 1
+	ALREADYEXISTT = 2;
+	ALREADYEXISTP = 3;
+)
+
+// ParseAnnounce es una representacion de una announce get query
+type ParseAnnounce struct {
 	InfoHash   string //dic bencodificado
 	PeerID     string // Id del peer
 	IP         net.IP //Ip del peer Opcional
-	Port       int16  // Port del peer Opcional
+	Port       int   // Port del peer Opcional
 	Uploaded   uint64
 	Downloaded uint64
 	Left       uint64
 	Event      string
 	NumWant    uint32 //Cantidad de peer que solicita el peer, Default 50
+	Request    bool // Indica si quiere recibir peers
 }
 
 // AnnounceResponse es una representacion de una respuesta a un announce get query
@@ -27,67 +36,182 @@ type AnnounceResponse struct {
 	Interval   uint16
 	TrackerID  string
 	Complete   uint64
-	Incomplete uint
+	Incomplete uint64
 	Peers      string
 }
 
-//PeersPool es un diccionario que mapea el peerAddr con un Peer
+//PeersPool es un diccionario que mapea el peerID con un Peer
 type PeersPool map[string]*PeerTk
 
 //PeerTk es la representacion de un Peer para la el Tracker
 type PeerTk struct {
 	Id         string
 	Addr       *net.TCPAddr
-	lastTime   time.Time
+	State      string  // Uno entre: seeder, started, completed or stopped
 	Uploaded   uint64
 	Downloaded uint64
 	leftDown   uint64
 }
 
-//TorrentsPool es un diccionario que mapea metainfoHash con el correspondiente torrent file
+//TorrentsPool es un diccionario que mapea metainfoHash con la informacion del torrent file
 type TorrentsPool map[string]*TorrentTk
 
 // TorrentTk es la representacion de un archivo torrent para el tracker
 type TorrentTk struct {
 	Hash       string
-	Name       string
 	Downloaded uint64
+	Complete   uint64
 	Peers      PeersPool
 }
 
 type TrackerServer struct {
-	AnnReq     AnnounceQuery
-	AnnResp    AnnounceResponse
+	//AnnReq     ParseAnnounce
+	//AnnResp    AnnounceResponse
 	TkID       string
 	Ip         net.IP
-	Port       string
+	Port       int32
 	ListenAddr *net.Listener
 	Torrents   TorrentsPool
 }
 
-func (tk *TrackerServer) Announce(ctx context.Context, annq *pb.AnnounceQuery) (*pb.AnnounceResponse, error) {
-
+func (tk *TrackerServer) Publish(ctx context.Context, pq *pb.PublishQuery) (*pb.PublishResponse, error){
+	ih := pq.GetInfoHash()
+	if ih == nil || len(ih) != 20{
+		err := fmt.Errorf("InfoHash invalido")
+		return nil, err
+	}
+	infoHash := string(ih[:])
+	peerID := pq.GetPeerID()
+	if peerID == ""{
+		err := fmt.Errorf("No se especifica el PeerID")
+		return nil, err
+	}
+	ip := net.ParseIP(pq.GetIP())
+	if ip == nil{
+		err := fmt.Errorf("Error con la IP")
+		return nil, err
+	}
+	port := pq.GetPort()
+	if port == 0{
+		err := fmt.Errorf("No se especifica el puerto")
+		return nil, err
+	}
+	status := tk.publishTorrent(infoHash, peerID, int(port), ip)
+	return &pb.PublishResponse{
+		Status:status,
+	}, nil
 }
 
-func AnnounceQueryCheck(annPb *pb.AnnounceQuery) (checedAnnounceQuery AnnounceQuery, err error) {
-	checkedAnnounceQ := make(AnnounceQuery)
+func (tk *TrackerServer) publishTorrent(infoHash, peerID string, port int, ip net.IP) int32 {
+	tp := tk.Torrents
+	if ttk, ok := tp[infoHash]; ok{
+		tpeers := ttk.Peers
+		if _, found := tpeers[peerID]; found{
+			return ALREADYEXISTP
+		}
+		tpeers[peerID] = &PeerTk{
+							Id:peerID, 
+							Addr:&net.TCPAddr{
+											IP:ip, 
+											Port:int(port)}, 
+							State:"seeder",} 
+		return ALREADYEXISTT
+	}
+	tp[infoHash] = &TorrentTk{
+		Hash:infoHash,
+		Downloaded:0,
+		Complete:1,
+		Peers: PeersPool{
+				 peerID:&PeerTk{
+				 		Id:peerID, 
+				 		Addr:&net.TCPAddr{
+								IP:ip, 
+								Port:port}, 
+						State:"seeder"},
+					    }}
+	return OK
+}
+
+//func parseConvert(port uint32) returns (string) {
+//	parsedPort := ":"
+//}
+
+
+
+func (tk *TrackerServer) Announce(ctx context.Context, annq *pb.AnnounceQuery) (*pb.AnnounceResponse, error) {
+	pa, err := AnnounceQueryCheck(annq)
+	var ar *pb.AnnounceResponse
+	if err != nil {
+		ar.FailureReason = err
+		return ar, err
+	}
+	event = pa.Event
+	infoHash = pa.InfoHash
+	
+	
+}
+
+func AnnounceQueryCheck(annPb *pb.AnnounceQuery) (pa ParseAnnounce, err error) {
 	infoHash := annPb.GetInfoHash()
-	if infoHash == "" {
+	if infoHash == nil|| len(infoHash) != 20{
 		err = fmt.Errorf("Infohash vacio")
 		return
 	}
-	checkedAnnounceQ.InfoHash = infoHash
-	peerID := annPb.GetPeerID
+	pa.InfoHash = string(infoHash[:])
+	peerID := annPb.GetPeerID()
 	if peerID == "" {
 		err = fmt.Errorf("No se especifica el PeerID")
 		return
 	}
-	checedAnnounceQuery.PeerID = peerID
-	ip := annPb.GetIP()
-	if ip == ""{
-		err = fmt.Errorf("No se especifica el Ip")
+	pa.PeerID = peerID
+	ip := net.ParseIP(annPb.GetIP())
+	if ip == nil{
+		err = fmt.Errorf("Error con la IP")
 		return
 	}
-	if 
+	pa.IP = ip
+	port := annPb.GetPort()
+	if port == 0 {
+		err = fmt.Errorf("No se especifica el puerto")
+		return
+	}
+	pa.Port = int(port)
+	uploaded := annPb.Uploaded
+	downloaded := annPb.Downloaded
+	left := annPb.Left
+	event := annPb.GetEvent()
+	numwant := annPb.GetNumWant()
+	if numwant < 1{
+		numwant = 40
+	}
+	if event != "public" && event != "started" && event != "completed" && event != "stopped"{
+		err = fmt.Errorf("No se reconoce el evento especificado")
+		return
+	}
+	pa.Event = event
+	if downloaded == nil && left == nil{
+		err = fmt.Errorf("No se especifican correctamente las claves downloaded y left")
+		return
+	}
+	pa.Downloaded = downloaded
+	pa.Uploaded = uploaded
+	pa.Left = left
+	return 
+}
 
+
+func (tk *TrackerServer) handlePublicEvent(pa *ParseAnnounce) {
+
+}
+
+func (tk *TrackerServer) handleStartedEvent(pa *ParseAnnounce) {
+	
+}
+
+func (tk *TrackerServer) handleCompletedEvent(pa *ParseAnnounce) {
+	
+}
+
+func (tk *TrackerServer) handleStopedEvent(pa *ParseAnnounce) {
+	
 }
