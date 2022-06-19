2ae9c1a545aa59b1e4e7192f75aadba5f008cd26
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-06-19 00:04:44 -0400

    add: kbucket and routingTablesFunctions

--- dht/Kademlia/routingTable.go
+++ dht/Kademlia/routingTable.go
@@ -20,13 +20,14 @@ const (
 type routingTable struct {
 	Self *NetworkNode
 	//routing table of nodes(160*k)
-	table [][]*node
+	table []*kbucket
 	//channel used to black routing table access
 	lock chan struct{}
 	//array with info of last time when a bucket was changed
 	lastChanged [B]time.Time
 }
 
+//create new instance of routingTable
 func newRoutingTable(options *Options) (*routingTable, error) {
 	rt := &routingTable{}
 
@@ -44,10 +45,10 @@ func newRoutingTable(options *Options) (*routingTable, error) {
 		rt.Self.ID = id
 	}
 	for i := 0; i <= B; i++ {
-		rt.resetRefreshTime(i)
+		rt.resetLastTimeChanged(i)
 	}
 	for i := 0; i <= B; i++ {
-		rt.table = append(rt.table, []*node{})
+		rt.table = append(rt.table, newKbucket())
 	}
 	if options.IP == "" || options.Port == "" {
 		return nil, errors.New("Port and IP required")
@@ -72,8 +73,43 @@ func (rt *routingTable) setPort(port string) error {
 	rt.Self.port = p
 	return nil
 }
-func (rt *routingTable) resetRefreshTime(bucket int) {
+func (rt *routingTable) resetLastTimeChanged(bucket int) {
 	<-rt.lock
-	rt.lastChanged[bucket] = time.Now()
+	rt.table[bucket].lastChanged = time.Now()
 	rt.lock <- struct{}{}
+}
+
+func (rt *routingTable) getLastTimeChanged(bucket int) time.Time {
+	<-rt.lock
+	defer func() { rt.lock <- struct{}{} }()
+	return rt.table[bucket].lastChanged
+}
+
+func (rt *routingTable) getTotalKnownNodes() int {
+	<-rt.lock
+	defer func() { rt.lock <- struct{}{} }()
+
+	total := 0
+	for _, bucket := range rt.table {
+		total += len(bucket.bucket)
+	}
+	return total
+}
+
+func (rt *routingTable) getNearestNodes(num int, node []byte) *nodeList {
+	return &nodeList{}
+}
+
+//index of correspondent bucket for node(first bite different)
+func (rt *routingTable) getFirstDifBitBucketIndex(node []byte) int {
+	for i := 0; i < len(node); i++ {
+		xor := node[i] ^ rt.Self.ID[i]
+		for j := 0; j < 8; j++ {
+			val := xor & (1 << (7 - j))
+			if val > 0 {
+				return B - 8*i + j - 1
+			}
+		}
+	}
+	return 0
 }
