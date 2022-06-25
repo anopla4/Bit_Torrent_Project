e1ba0e50b3a38290c4f3ebff5c76963b417bf037
abel1927 <abel.molina@estudiantes.matcom.uh.cu>
2022-06-25 14:24:23 -0400

    delete: unnecesary file

--- tracker/tracker.go
+++
@@ -1,9 +0,0 @@
-package tracker
-
-//import "github.com/anoppa/Bit_Torrent_Project/tracker/comunication"
-
-// Tracker exported
-type Tracker struct {
-	AnnReq  AnnounceQuery
-	AnnResp AnnounceResponse
-}
