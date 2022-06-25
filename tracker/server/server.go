e1190d8a658a7cff7452c6d41444a72f045784f4
abel1927 <abel.molina@estudiantes.matcom.uh.cu>
2022-06-25 14:24:23 -0400

    delete: unnecesary file

--- tracker/server/server.go
+++ tracker/server/server.go
@@ -356,6 +356,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	fmt.Println(fmt.Sprintf("Server in port:%d",*port))  
 	sr := grpc.NewServer()
 
 	pb.RegisterTrackerServer(sr, newTrackerServer())
