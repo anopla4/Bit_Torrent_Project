d735cc9c5aa42c03f9b93aaea495fb64eae3ac2b
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-06-25 14:16:09 -0400

    feat: Set TLS certificates in peers connections

--- Client/torrent_peer/uploader_client/uploader_client.go
+++ Client/torrent_peer/uploader_client/uploader_client.go
@@ -2,6 +2,8 @@ package uploader_client
 
 import (
 	"bufio"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"log"
 	"net"
@@ -10,34 +12,71 @@ import (
 )
 
 func ServerTCP(port string) error {
-	l, listenErr := net.Listen("tcp", port)
+	// Setting SSL certificate
+	cert, err := tls.LoadX509KeyPair("./SSL/cert.pem", "./SSL/key.pem")
+	if err != nil {
+		log.Fatalf("Error loading certificate: %v\n", err)
+	}
+	tlsCfg := &tls.Config{Certificates: []tls.Certificate{cert}}
+
+	l, listenErr := tls.Listen("tcp", port, tlsCfg)
 	if listenErr != nil {
 		fmt.Println(listenErr)
 		return listenErr
 	}
 	defer l.Close()
 
-	c, err := l.Accept()
+	for {
+		c, err := l.Accept()
 
-	if err != nil {
-		log.Fatalf("Error while accepting next connection: %v\n", err)
+		if err != nil {
+			log.Fatalf("Error while accepting next connection: %v\n", err)
+			return err
+		}
+		log.Printf("Connection accepted: %v\n", c)
+
+		tlsCon, ok := c.(*tls.Conn)
+		if ok {
+			log.Println("ok = true")
+			state := tlsCon.ConnectionState()
+			for _, v := range state.PeerCertificates {
+				log.Print(x509.MarshalPKIXPublicKey(v.PublicKey))
+			}
+		}
+
+		errChan := make(chan error)
+		go HandleTCPConnection(c, errChan)
+
+		go func(errChan chan error) {
+			for {
+				if connErr, ok := <-errChan; ok {
+					fmt.Println(connErr)
+					break
+				} else {
+					break
+				}
+			}
+		}(errChan)
 	}
+}
 
+func HandleTCPConnection(c net.Conn, errorChan chan error) {
 	var buf = bufio.NewReader(c)
 	for {
-		deadlineErr := c.SetReadDeadline(time.Now().Add(3 * time.Second))
+		deadlineErr := c.SetReadDeadline(time.Now().Add(10 * time.Second))
 		if deadlineErr != nil {
 			fmt.Println(deadlineErr)
-			return deadlineErr
+			errorChan <- deadlineErr
+			break
 		}
 		netData, err := buf.ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
-			return err
+			errorChan <- err
+			break
 		}
 		if strings.TrimSpace(string(netData)) == "STOP" {
 			fmt.Println("Exiting TCP server!")
-			return nil
 		}
 
 		fmt.Print("-> ", string(netData))
@@ -47,7 +86,8 @@ func ServerTCP(port string) error {
 
 		if writeErr != nil {
 			log.Fatalf("Error while writing to the connection: %v\n", writeErr)
-			return writeErr
+			errorChan <- writeErr
+			break
 		}
 
 	}
