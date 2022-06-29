2fc565a905ffadcfaf2d2c7759c7348b387a1691
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-06-29 17:40:22 -0400

    add: support multiple peers_info for each infohash in kStore

--- dht/Kademlia/kStore.go
+++ dht/Kademlia/kStore.go
@@ -1,14 +1,15 @@
 package dht
 
 import (
-	"bytes"
 	"errors"
+	"sort"
+	"strings"
 	"time"
 )
 
-// KStore struct for data storage
+// KStore struct for data storage in kademlia
 type KStore struct {
-	data              map[string][]byte //map info_hash to data
+	data              map[string][]string //map info_hash to data
 	lock              chan struct{}
 	expirationTimeMap map[string]time.Time //map key to expiration time
 	republishTimeMap  map[string]time.Time //map key to republish time
@@ -18,13 +19,24 @@ type KStore struct {
 func NewKStore() *KStore {
 	st := &KStore{}
 
-	st.data = map[string][]byte{}
+	st.data = map[string][]string{}
 	st.lock = make(chan struct{})
 	st.lock <- struct{}{}
 	st.expirationTimeMap = map[string]time.Time{}
 	st.republishTimeMap = map[string]time.Time{}
 
 	return st
+}
+
+func (st *KStore) checkKeyValuePair(key string, value string) bool {
+	if values, ok := st.data[key]; ok {
+		for _, valueC := range values {
+			if value == valueC {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 // Store add a new key, value pair to the storage
@@ -35,33 +47,51 @@ func (st *KStore) Store(key []byte, data []byte, expirationTime time.Time, repub
 		return errors.New("expiration time and republish time have to be after current time")
 	}
 	keyString := string(key)
-	if dataCurrent, in := st.data[keyString]; in {
-		st.expirationTimeMap[keyString] = expirationTime
-		st.republishTimeMap[keyString] = republishTime
-		if !bytes.Equal(dataCurrent, data) {
-			st.data[keyString] = data
-		}
+	dataString := string(data)
+	//combine key and data for time maps
+	keyKD := keyString + "/data:" + dataString
+	if _, in := st.expirationTimeMap[keyKD]; in {
+		st.expirationTimeMap[keyKD] = expirationTime
+		st.republishTimeMap[keyKD] = republishTime
+		// if !st.checkKeyValuePair(keyString, dataString) {
+		// 	st.data[keyString] = append(st.data[keyString], dataString)
+		// }
 		return nil
 	}
-
-	st.data[keyString] = data
+	if _, ok := st.data[keyString]; ok {
+		// if !st.checkKeyValuePair(keyString, dataString) {
+		// 	st.data[keyString] = append(st.data[keyString], dataString)
+		// }
+		index := sort.SearchStrings(st.data[keyString], dataString)
+		if index == len(st.data[keyString]) {
+			st.data[keyString] = append(st.data[keyString], dataString)
+		} else {
+			if st.data[keyString][index] != dataString {
+				st.data[keyString] = append(st.data[keyString][:index], append([]string{dataString}, st.data[keyString][index:]...)...)
+			}
+		}
+		st.expirationTimeMap[keyString] = expirationTime
+		st.republishTimeMap[keyString] = republishTime
+		return nil
+	}
+	st.data[keyString] = []string{dataString}
 	st.expirationTimeMap[keyString] = expirationTime
 	st.republishTimeMap[keyString] = republishTime
 	return nil
 }
 
 // GetValue return data for given key, error if the key is not in storage
-func (st *KStore) GetValue(key []byte) ([]byte, error) {
+func (st *KStore) GetValues(key []byte) ([]string, error) {
 	<-st.lock
 	defer func() { st.lock <- struct{}{} }()
 	if data, in := st.data[string(key)]; in {
 		return data, nil
 	}
-	return []byte{}, errors.New("Not value found for given key")
+	return []string{}, errors.New("Not value found for given key")
 }
 
 // GetAllPairs return all pairs in store
-func (st *KStore) GetAllPairs() (map[string][]byte, error) {
+func (st *KStore) GetAllPairs() (map[string][]string, error) {
 	<-st.lock
 	defer func() { st.lock <- struct{}{} }()
 	return st.data, nil
@@ -74,8 +104,31 @@ func (st *KStore) Remove(key []byte) bool {
 	keyString := string(key)
 	if _, in := st.data[keyString]; in {
 		delete(st.data, keyString)
-		delete(st.republishTimeMap, keyString)
-		delete(st.expirationTimeMap, keyString)
+		for k, _ := range st.expirationTimeMap {
+			if strings.Split(k, "/data:")[0] == keyString {
+				delete(st.republishTimeMap, k)
+				delete(st.expirationTimeMap, k)
+			}
+		}
+		return true
+	}
+	return false
+}
+
+func (st *KStore) RemoveKeyValue(key string, value string) bool {
+	<-st.lock
+	defer func() { st.lock <- struct{}{} }()
+	if _, in := st.data[key]; in {
+		index := sort.SearchStrings(st.data[key], value)
+		if index == len(st.data[key]) || st.data[key][index] != value {
+			return false
+		} else {
+			if len(st.data[key]) == 1 {
+				delete(st.data, key)
+			} else {
+				st.data[key] = append(st.data[key][:index], st.data[key][index+1:]...)
+			}
+		}
 		return true
 	}
 	return false
@@ -86,26 +139,35 @@ func (st *KStore) ExpireKeys() error {
 	<-st.lock
 	defer func() { st.lock <- struct{}{} }()
 
-	for key, expTime := range st.expirationTimeMap {
+	for keyKD, expTime := range st.expirationTimeMap {
 		if time.Now().After(expTime) {
-			delete(st.data, key)
-			delete(st.expirationTimeMap, key)
-			delete(st.republishTimeMap, key)
+			// delete(st.data, key)
+			keyS := strings.Split(keyKD, "/data:")
+			key := keyS[0]
+			value := keyS[1]
+			st.RemoveKeyValue(key, value)
+			delete(st.expirationTimeMap, keyKD)
+			delete(st.republishTimeMap, keyKD)
 		}
 	}
 	return nil
 }
 
-// GetKeysToRepublish return key wich repTime is before time.Now
-func (st *KStore) GetKeysToRepublish() ([][]byte, error) {
+// GetKeyValuesToRepublish return string of the form key and values wich repTime is before time.Now
+func (st *KStore) GetKeyValuesToRepublish() ([]string, []string, error) {
 	<-st.lock
 	defer func() { st.lock <- struct{}{} }()
 
-	keysToRepublish := [][]byte{}
-	for key, repTime := range st.republishTimeMap {
+	keysToRepublish := []string{}
+	valueToRepublish := []string{}
+	for keyKD, repTime := range st.republishTimeMap {
 		if time.Now().After(repTime) {
-			keysToRepublish = append(keysToRepublish, []byte(key))
+			aux := strings.Split(keyKD, "/data:")
+			key := aux[0]
+			value := aux[1]
+			keysToRepublish = append(keysToRepublish, key)
+			valueToRepublish = append(valueToRepublish, value)
 		}
 	}
-	return keysToRepublish, nil
+	return keysToRepublish, valueToRepublish, nil
 }
