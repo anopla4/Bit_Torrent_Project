b21f8b6daaec9a40647027c23889a6ceff17baf2
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-06-30 17:46:41 -0400

    add: CompactInfo manipulation for node struct

--- dht/Kademlia/dht.go
+++ dht/Kademlia/dht.go
@@ -1,5 +1,11 @@
 package dht
 
+import (
+	"net"
+	"strconv"
+	"time"
+)
+
 type dht struct {
 	routingTable *routingTable
 	store        KStore
@@ -7,26 +13,42 @@ type dht struct {
 	// sendedToken  map[string]string
 }
 type Options struct {
-	ID   []byte
-	IP   string
-	Port string
+	ID             []byte
+	IP             string
+	Port           string
+	ExpirationTime time.Duration
+	RepublishTime  time.Duration
 }
 
 func (dht *dht) Update(node *node) {
-	//TODO
+	dht.routingTable.updateBucketOfNode(node)
 }
 
-//TODO
+// FindNode return a list with compactInfo of k nearest node to target
 func (dht *dht) FindNode(target []byte) []string {
-	return []string{}
+	nl := dht.routingTable.getNearestNodes(K, target)
+	nodesInfo := []string{}
+	for _, n := range nl.Nodes {
+		nodesInfo = append(nodesInfo, string(newNode(n).CompactInfo()))
+	}
+	return nodesInfo
 }
 
-//TODO
+// GetPeers returns addr of peer containing the infohash in case it is in the storage or k nearest nodes in other case(also true in first casem false otherwise)
 func (dht *dht) GetPeers(infohash []byte) ([]string, bool) {
-	return []string{}, true
+	if values, ok := dht.store.data[string(infohash)]; ok {
+		return values, true
+	}
+	nodes := dht.FindNode(infohash)
+	return nodes, false
 }
 
-//TODO
+// Store save info in dht storage
 func (dht *dht) Store(infohash []byte, ip string, port string) error {
-	return nil
+	expTime := time.Now().Add(dht.options.ExpirationTime)
+	repTime := time.Now().Add(dht.options.RepublishTime)
+	portI, _ := strconv.Atoi(port)
+	addr := net.UDPAddr{IP: net.ParseIP(ip), Port: portI}
+	err := dht.store.Store(infohash, []byte(addr.String()), expTime, repTime)
+	return err
 }
