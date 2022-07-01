2f9d2dc2227f592e4e9f804de4cfa826cfb15af5
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-07-01 18:31:12 -0400

    refactor: main

--- Client/main.go
+++ Client/main.go
@@ -1,89 +1,30 @@
 package main
 
 import (
-	"Bit_Torrent_Project/client/client/structures"
-	"Bit_Torrent_Project/client/torrent_peer/downloader_client"
+	"Bit_Torrent_Project/client/client/torrent_file"
 	"Bit_Torrent_Project/client/torrent_peer/uploader_client"
 	"fmt"
 	"log"
 	"os"
-	"sync"
-	"time"
 )
 
-var wg = sync.WaitGroup{}
-
 func main() {
-	wg.Add(2)
-
 	arguments := os.Args
 
-	requests := make(chan *structures.Message, 50)
-	errorChan := make(chan error)
-	go StartClientUploader(":"+arguments[1], errorChan)
+	torrentPath := arguments[1]
+	downloadTo := arguments[2]
 
-	fmt.Println("Type start")
-	var start string
-	_, err := fmt.Scanln(&start)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if start == "s" {
-		go StartClientDownloader(arguments[2], requests, errorChan)
-	}
-
-	if err, ok := <-errorChan; ok {
-		fmt.Println(err)
-	}
-
-	wg.Wait()
-}
-
-func StartClientDownloader(url string, requests chan *structures.Message, errChan chan error) {
-	// Dial throw port and ip from peer
-	cc, err := downloader_client.StartClientTCP(url)
+	torrentFile, err := torrent_file.OpenTorrentFile(torrentPath)
 
 	if err != nil {
-		log.Fatalf("Could not connect: %v", err)
-		return
+		log.Printf("Error while parsing torrent: %v\n", err)
 	}
-	defer cc.Close()
 
-	// Start message stream through TCP connection
-	// requestsErr := make(chan error)
-	// responsesErr := make(chan error)
+	err = torrentFile.DownloadTo(downloadTo)
 
-	wgReqResp := sync.WaitGroup{}
-	requests <- &structures.Message{
-		ID: structures.HAVE,
+	if err != nil {
+		log.Printf("Error while downloading torrent: %v\n", err)
 	}
-	wgReqResp.Add(2)
-	go func() {
-		downloader_client.SendRequests(cc, requests, errChan)
-		wgReqResp.Done()
-	}()
-
-	go func() {
-		downloader_client.ReceiveResponses(cc, errChan)
-		wgReqResp.Done()
-	}()
-
-	time.Sleep(4 * time.Second)
-	requests <- &structures.Message{
-		ID: structures.HAVE,
-	}
-	wgReqResp.Wait()
-	go func() {
-		for {
-			if err, ok := <-errChan; ok {
-				fmt.Println(err)
-			} else {
-				break
-			}
-		}
-	}()
-	wg.Done()
 }
 
 func StartClientUploader(port string, errChan chan error) {
@@ -96,6 +37,4 @@ func StartClientUploader(port string, errChan chan error) {
 		log.Fatalf("Error while starting server: %v\n", err)
 		return
 	}
-
-	wg.Done()
 }
