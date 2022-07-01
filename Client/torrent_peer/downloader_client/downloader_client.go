8b04c8888304413a882e189096fb676016c19e08
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-07-01 18:27:30 -0400

    fix: Remove requests sending from downloader_client

--- Client/torrent_peer/downloader_client/downloader_client.go
+++ Client/torrent_peer/downloader_client/downloader_client.go
@@ -1,62 +1,39 @@
 package downloader_client
 
 import (
-	"Bit_Torrent_Project/client/client/structures"
-	"bufio"
+	"Bit_Torrent_Project/client/client/communication"
 	"crypto/tls"
-	"fmt"
 	"log"
 	"net"
 )
 
-func SendRequests(c net.Conn, requests chan *structures.Message, errors chan error) {
-	for {
-		if req, ok := <-requests; ok {
-			serializedReq := req.Serialize()
-			log.Println(serializedReq)
-			_, err := c.Write(serializedReq)
-			if err != nil {
-				log.Fatalf("Error while sending request: %v\n", err)
-				errors <- err
-				break
-			}
-		} else {
-			break
-		}
+func SendHandshake(infoHash [20]byte, peerId [20]byte, c net.Conn, errChan chan error) {
+	hsh := communication.Handshake{Pstr: "BitTorrent protocol", PeerId: peerId, InfoHash: infoHash}
+	serializedHsh := hsh.Serialize()
+	log.Println(serializedHsh)
+	_, err := c.Write(serializedHsh)
+	if err != nil {
+		log.Fatalf("Error while sending request: %v\n", err)
+		errChan <- err
 	}
 }
 
-func ReceiveResponses(c net.Conn, errors chan error) {
-	for {
-		message, err := structures.Deserialize(bufio.NewReader(c))
-		if err != nil {
-			errors <- err
-			break
-		}
-		log.Fatalln(message)
-	}
-}
-
-func StartClientTCP(url string) (net.Conn, error) {
+func StartClientTCP(url string, errChan chan error) net.Conn {
 	cert, tlsErr := tls.LoadX509KeyPair("./SSL/client.pem", "./SSL/client.key")
 
 	if tlsErr != nil {
-		log.Fatalf("Error while loading tls keys: %v\n", tlsErr)
+		log.Printf("Error while loading tls keys: %v\n", tlsErr)
+		errChan <- tlsErr
+		return nil
 	}
 
 	tlsConfig := &tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
 	c, err := tls.Dial("tcp", url, tlsConfig)
 	if err != nil {
-		return nil, err
+		errChan <- err
+		return nil
 	}
 	log.Println("client: connected to: ", c.RemoteAddr())
 
-	state := c.ConnectionState()
-	for _, v := range state.PeerCertificates {
-		// fmt.Println(x509.MarshalPKIXPublicKey(v.PublicKey))
-		fmt.Println(v.Subject)
-	}
-	log.Println("client: handshake: ", state.HandshakeComplete)
-
-	return c, nil
+	return c
 }
