5946dda7a7cb8484f321550bf33492ae200438da
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-07-02 16:22:53 -0400

    feat: Initial handshake and bitfield message in peers connection

--- Client/torrent_peer/downloader_client/downloader_client.go
+++ Client/torrent_peer/downloader_client/downloader_client.go
@@ -5,20 +5,25 @@ import (
 	"crypto/tls"
 	"log"
 	"net"
+	"time"
 )
 
-func SendHandshake(infoHash [20]byte, peerId [20]byte, c net.Conn, errChan chan error) {
+func SendHandshake(c net.Conn, infoHash [20]byte, peerId string) error {
+	_ = c.SetDeadline(time.Now().Add(3 * time.Second))
+	defer c.SetDeadline(time.Time{}) // Disable the deadline
+
 	hsh := communication.Handshake{Pstr: "BitTorrent protocol", PeerId: peerId, InfoHash: infoHash}
 	serializedHsh := hsh.Serialize()
 	log.Println(serializedHsh)
 	_, err := c.Write(serializedHsh)
 	if err != nil {
 		log.Fatalf("Error while sending request: %v\n", err)
-		errChan <- err
+		return err
 	}
+	return nil
 }
 
-func StartClientTCP(url string, errChan chan error) net.Conn {
+func StartConnection(url string, errChan chan error) net.Conn {
 	cert, tlsErr := tls.LoadX509KeyPair("./SSL/client.pem", "./SSL/client.key")
 
 	if tlsErr != nil {
@@ -36,4 +41,16 @@ func StartClientTCP(url string, errChan chan error) net.Conn {
 	log.Println("client: connected to: ", c.RemoteAddr())
 
 	return c
+}
+
+func StartClientTCP(url string, infoHash [20]byte, peerID string, errChan chan error) (net.Conn, error) {
+	c := StartConnection(url, errChan)
+	hshErr := SendHandshake(c, infoHash, peerID)
+
+	if hshErr != nil {
+		errChan <- hshErr
+		return nil, hshErr
+	}
+
+	return c, nil
 }
