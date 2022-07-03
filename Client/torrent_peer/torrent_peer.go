f3ad6b7865f3b8761fe11ffd696ee56808f17461
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-07-03 16:37:38 -0400

    feat: Choke peers from client and server

--- Client/torrent_peer/torrent_peer.go
+++ Client/torrent_peer/torrent_peer.go
@@ -4,40 +4,63 @@ import (
 	"Bit_Torrent_Project/client/client/communication"
 	"Bit_Torrent_Project/client/client/peer"
 	"Bit_Torrent_Project/client/torrent_peer/downloader_client"
+	"Bit_Torrent_Project/client/torrent_peer/uploader_client"
+	"context"
 	"fmt"
+	"log"
 	"net"
 	"strconv"
 	"time"
+	"trackerpb"
 )
 
 var MaxBlocks = 5
 var MaxBlockSize = 16384
 
 type Client struct {
-	Connection net.Conn
-	Peer       peer.Peer
-	Choked     bool
-	Interested bool
-	Bitfield   communication.Bitfield
-	InfoHash   [20]byte
-	PeerId     string
+	Connection     net.Conn
+	Peer           peer.Peer
+	Choked         bool
+	PeerChoked     bool
+	Interested     bool
+	PeerInterested bool
+	Bitfield       communication.Bitfield
+	InfoHash       [20]byte
+	PeerId         string
+}
+
+type ConnectionsState struct {
+	LastUpload                    map[string]time.Time
+	NumberOfBlocksInLast30Seconds map[string]int
+	NumberOfUploadPeers           int
 }
 
 func (c *Client) SendUnchoke() error {
 	_, err := c.Connection.Write((&communication.Message{ID: communication.UNCHOKE}).Serialize())
+	if err != nil {
+		c.PeerChoked = false
+	}
 	return err
 }
 func (c *Client) SendChoke() error {
 	_, err := c.Connection.Write((&communication.Message{ID: communication.CHOKE}).Serialize())
+	if err != nil {
+		c.PeerChoked = true
+	}
 	return err
 }
 func (c *Client) SendInterested() error {
-	c.Interested = true
 	_, err := c.Connection.Write((&communication.Message{ID: communication.INTERESTED}).Serialize())
+	if err != nil {
+		c.Interested = true
+	}
 	return err
 }
 func (c *Client) SendNotInterested() error {
 	_, err := c.Connection.Write((&communication.Message{ID: communication.NOTINTERESTED}).Serialize())
+	if err != nil {
+		c.Interested = false
+	}
 	return err
 }
 func (c *Client) SendHave(index int) error {
@@ -71,7 +94,7 @@ func RecvBitfield(conn net.Conn) (communication.Bitfield, error) {
 	return msg.Payload, nil
 }
 
-func StartConnectionWithPeer(peer peer.Peer, infoHash [20]byte, peerId string, errChan chan error) (*Client, error) {
+func StartConnectionWithPeer(peer peer.Peer, infoHash [20]byte, peerId string, peers []*Client, errChan chan error) (*Client, error) {
 	c, err := downloader_client.StartClientTCP(string(peer.IP)+":"+strconv.Itoa(int(peer.Port)), infoHash, peerId, errChan)
 	if err != nil {
 		errChan <- err
@@ -87,7 +110,7 @@ func StartConnectionWithPeer(peer peer.Peer, infoHash [20]byte, peerId string, e
 		return nil, bfErr
 	}
 
-	return &Client{
+	client := &Client{
 		Connection: c,
 		Choked:     true,
 		Interested: false,
@@ -95,13 +118,17 @@ func StartConnectionWithPeer(peer peer.Peer, infoHash [20]byte, peerId string, e
 		Peer:       peer,
 		InfoHash:   infoHash,
 		PeerId:     peerId,
-	}, nil
+	}
+
+	peers = append(peers, client)
+
+	return client, nil
 
 }
 
-func DownloadPiece(c *Client, task *pieceTask) (*pieceResult, error) {
+func DownloadPiece(c *Client, task *PieceTask, cs *ConnectionsState) (*pieceResult, error) {
 	pp := pieceProgress{
-		index:  task.index,
+		index:  task.Index,
 		client: c,
 		buf:    make([]byte, task.length),
 	}
@@ -123,7 +150,7 @@ func DownloadPiece(c *Client, task *pieceTask) (*pieceResult, error) {
 					blockSize = task.length - pp.requested
 				}
 
-				err := c.SendRequest(task.index, pp.requested, blockSize)
+				err := c.SendRequest(task.Index, pp.requested, blockSize)
 				if err != nil {
 					return nil, err
 				}
@@ -144,6 +171,10 @@ func DownloadPiece(c *Client, task *pieceTask) (*pieceResult, error) {
 			c.Choked = false
 		case communication.CHOKE:
 			c.Choked = true
+		case communication.INTERESTED:
+			c.PeerInterested = true
+		case communication.NOTINTERESTED:
+			c.PeerInterested = false
 		case communication.HAVE:
 			index, err := communication.ParseHave(*msg)
 			if err != nil {
@@ -157,23 +188,35 @@ func DownloadPiece(c *Client, task *pieceTask) (*pieceResult, error) {
 			}
 			pp.downloaded += n
 			pp.pendingBlocks--
+			if time.Now().Sub(cs.LastUpload[c.Peer.Id]) > time.Duration(30*time.Second) {
+				cs.LastUpload[c.Peer.Id] = time.Now()
+				cs.NumberOfBlocksInLast30Seconds[c.Peer.Id] = 0
+			} else {
+				cs.LastUpload[c.Peer.Id] = time.Now()
+				cs.NumberOfBlocksInLast30Seconds[c.Peer.Id]++
+			}
 		}
 	}
 
-	return &pieceResult{index: task.index, piece: pp.buf}, nil
+	return &pieceResult{index: task.Index, piece: pp.buf}, nil
 }
 
-func (t *Torrent) StartPeerDownload(peer peer.Peer, tasks chan *pieceTask, responses chan *pieceResult, errChan chan error) {
+func (t *Torrent) StartPeerDownload(dp *downloadProgress, peer peer.Peer, tasks chan *PieceTask, responses chan *pieceResult,
+	tc trackerpb.TrackerClient, IP net.IP, port string, peers []*Client, servers []*uploader_client.Server, cs *ConnectionsState,
+	errChan chan error) {
 	// Dial throw port and ip from peer
-	client, err := StartConnectionWithPeer(peer, t.InfoHash, t.PeerId, errChan)
+	client, err := StartConnectionWithPeer(peer, t.InfoHash, t.PeerId, peers, errChan)
 
 	if err != nil {
 		errChan <- err
 		return
 	}
-	defer client.Connection.Close()
+	defer func() {
+		_ = client.SendNotInterested()
+		client.Connection.Close()
+	}()
 
-	// Wait for being unchoked and send interested message
+	// Send Unchoke and Interested message
 	err = client.SendUnchoke()
 	if err != nil {
 		errChan <- err
@@ -186,16 +229,41 @@ func (t *Torrent) StartPeerDownload(peer peer.Peer, tasks chan *pieceTask, respo
 		return
 	}
 
+	p, _ := strconv.Atoi(port)
+	left := dp.left
+	announce := trackerpb.AnnounceQuery{
+		InfoHash: t.InfoHash[:],
+		PeerID:   t.PeerId,
+		IP:       IP.String(),
+		Port:     int32(p),
+		Event:    "started",
+		Left:     uint64(dp.left),
+	}
+	ctx := context.Background()
+
+	tRes, tResErr := tc.Announce(ctx, &announce)
+
+	if tResErr != nil {
+		errChan <- err
+		log.Printf("Error in tracker's Announce response: %v\n", tResErr)
+		return
+	}
+	interval := tRes.GetInterval()
+
+	go t.KeepAliveToTracker(interval, IP, p, &left, dp, ctx, tc)
+
 	for {
 		if task, ok := <-tasks; ok {
 			// Ask for piece
-			if !client.Bitfield.HasPiece(task.index) {
+			if !client.Bitfield.HasPiece(task.Index) {
 				tasks <- task
 				continue
 			}
 
+			dp.requested += task.length
+
 			// Download piece
-			piece, err := DownloadPiece(client, task)
+			piece, err := DownloadPiece(client, task, cs)
 			if err != nil {
 				errChan <- err
 				tasks <- task
@@ -209,11 +277,54 @@ func (t *Torrent) StartPeerDownload(peer peer.Peer, tasks chan *pieceTask, respo
 				continue
 			}
 
-			client.SendHave(task.index)
+			dp.bitfield.SetPiece(task.Index)
+			dp.pendingPieces--
+			dp.downloaded += task.length
+			dp.requested -= task.length
+			left -= uint64(task.length)
+			_ = client.SendHave(task.Index)
+
+			announce := trackerpb.AnnounceQuery{
+				InfoHash:   t.InfoHash[:],
+				PeerID:     t.PeerId,
+				IP:         IP.String(),
+				Port:       int32(p),
+				Event:      "request",
+				Left:       left,
+				Request:    false,
+				Downloaded: uint64(dp.downloaded),
+			}
+
+			tRes, tResErr = tc.Announce(ctx, &announce)
 			responses <- piece
 		} else {
 			break
 		}
+	}
+
+}
+func (t *Torrent) KeepAliveToTracker(interval uint32, IP net.IP, port int, left *uint64, dp *downloadProgress, ctx context.Context, tc trackerpb.TrackerClient) {
+
+	for {
+		announce := trackerpb.AnnounceQuery{
+			InfoHash:   t.InfoHash[:],
+			PeerID:     t.PeerId,
+			IP:         IP.String(),
+			Port:       int32(port),
+			Event:      "request",
+			Left:       *left,
+			Request:    false,
+			Downloaded: uint64(dp.downloaded),
+		}
+
+		_, tResErr := tc.Announce(ctx, &announce)
+
+		if tResErr != nil {
+			log.Printf("Error in tracker keepalive: %v\n", tResErr)
+			return
+		}
+
+		time.Sleep(time.Duration(interval) * 1000000000)
 	}
 
 }
