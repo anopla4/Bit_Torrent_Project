87040e72a2a70dc6c97c2e6de67485f479e64eaa
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-07-03 23:58:43 -0400

    fix: Errors

--- Client/main.go
+++ Client/main.go
@@ -2,39 +2,101 @@ package main
 
 import (
 	"Bit_Torrent_Project/client/client/torrent_file"
+	"Bit_Torrent_Project/client/client/tracker_communication"
+	"Bit_Torrent_Project/client/torrent_peer"
 	"Bit_Torrent_Project/client/torrent_peer/uploader_client"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"sync"
+	"time"
 )
 
+var wg = sync.WaitGroup{}
+
 func main() {
+	// peerID := randstr.Hex(10)
+
 	arguments := os.Args
 
-	torrentPath := arguments[1]
-	downloadTo := arguments[2]
+	peerId := arguments[1]
 
+	info := LoadInfo("./info.json")
+	errChan := make(chan error)
+	csServer := &torrent_peer.ConnectionsState{LastUpload: map[string]time.Time{}, NumberOfBlocksInLast30Seconds: map[string]int{}}
+	wg.Add(1)
+	go StartClientUploader(info, peerId, csServer, errChan)
+	// Start downloader
+	var download string
+	_, err := fmt.Scanln(&download)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if download == "d" {
+		fmt.Println("Waiting for paths to download...")
+		var torrentPath, downloadTo string
+		_, err := fmt.Scanln(&torrentPath, &downloadTo)
+		if err != nil {
+			log.Fatal(err)
+		}
+		wg.Add(1)
+		go Download(torrentPath, downloadTo, peerId)
+	}
+	wg.Wait()
+	// Publish("../a.torrent", "192.168.169.32:8167", peerId, "192.168.169.14")
+	// _ = torrent_file.BuildTorrentFile("../test.txt", "../a.torrent", "192.168.169.32:8167")
+}
+
+func Download(torrentPath string, downloadTo string, peerId string) {
 	torrentFile, err := torrent_file.OpenTorrentFile(torrentPath)
+	fmt.Println("Pieces hashes:", torrentFile.Info.Pieces)
+	csClient := &torrent_peer.ConnectionsState{LastUpload: map[string]time.Time{}, NumberOfBlocksInLast30Seconds: map[string]int{}}
 
 	if err != nil {
 		log.Printf("Error while parsing torrent: %v\n", err)
 	}
-
-	err = torrentFile.DownloadTo(downloadTo)
+	err = torrentFile.DownloadTo(downloadTo, csClient, peerId)
 
 	if err != nil {
 		log.Printf("Error while downloading torrent: %v\n", err)
 	}
 }
 
-func StartClientUploader(port string, errChan chan error) {
+func StartClientUploader(info *uploader_client.ClientInfo, peerID string, cs *torrent_peer.ConnectionsState, errChan chan error) {
 	fmt.Println("Hello World")
 
 	// Listen to new connections
-	err := uploader_client.ServerTCP(port, errChan)
+	err := uploader_client.ServerTCP(info, peerID, cs, errChan)
 
 	if err != nil {
 		log.Fatalf("Error while starting server: %v\n", err)
 		return
 	}
+	wg.Done()
+}
+
+func LoadInfo(path string) *uploader_client.ClientInfo {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	var buf uploader_client.ClientInfo
+	err = json.Unmarshal(content, &buf)
+	if err != nil {
+		log.Fatalf("Error during Unmarshal(): %v\n", err)
+	}
+	return &buf
+}
+
+func Publish(path string, trackerUrl string, peerId string, IP string) {
+	tf, tfErr := torrent_file.OpenTorrentFile(path)
+	if tfErr != nil {
+		log.Printf("Error while opening torrent file %v\n", tfErr)
+		return
+	}
+	trRes := tracker_communication.PublishTorrent(trackerUrl, tf.Info.InfoHash, peerId, IP)
+	fmt.Printf("Tracker status response from Publish: %v\n", trRes.GetStatus())
 }
--- Client/torrent_peer/downloader_client/downloader_client.go
+++ Client/torrent_peer/downloader_client/downloader_client.go
@@ -3,21 +3,21 @@ package downloader_client
 import (
 	"Bit_Torrent_Project/client/client/communication"
 	"crypto/tls"
+	"errors"
+	"fmt"
 	"log"
 	"net"
-	"time"
 )
 
 func SendHandshake(c net.Conn, infoHash [20]byte, peerId string) error {
-	_ = c.SetDeadline(time.Now().Add(3 * time.Second))
-	defer c.SetDeadline(time.Time{}) // Disable the deadline
+	// _ = c.SetDeadline(time.Now().Add(10 * time.Second))
+	// defer c.SetDeadline(time.Time{}) // Disable the deadline
 
 	hsh := communication.Handshake{Pstr: "BitTorrent protocol", PeerId: peerId, InfoHash: infoHash}
 	serializedHsh := hsh.Serialize()
-	log.Println(serializedHsh)
 	_, err := c.Write(serializedHsh)
 	if err != nil {
-		log.Fatalf("Error while sending request: %v\n", err)
+		log.Fatalf("Error while sending handshake: %v\n", err)
 		return err
 	}
 	return nil
@@ -33,23 +33,37 @@ func StartConnection(url string, errChan chan error) net.Conn {
 	}
 
 	tlsConfig := &tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	fmt.Printf("Dialing to %s...\n", url)
 	c, err := tls.Dial("tcp", url, tlsConfig)
 	if err != nil {
 		errChan <- err
 		return nil
 	}
-	log.Println("client: connected to: ", c.RemoteAddr())
+	log.Println("client: connected")
 
 	return c
 }
 
-func StartClientTCP(url string, infoHash [20]byte, peerID string, errChan chan error) (net.Conn, error) {
+func StartClientTCP(url string, infoHash [20]byte, id string, peerId string, errChan chan error) (net.Conn, error) {
 	c := StartConnection(url, errChan)
-	hshErr := SendHandshake(c, infoHash, peerID)
+	hshErr := SendHandshake(c, infoHash, id)
 
 	if hshErr != nil {
 		errChan <- hshErr
 		return nil, hshErr
+	}
+
+	hsh, err := communication.DeserializeHandshake(c)
+
+	if err != nil {
+		errChan <- hshErr
+		return nil, hshErr
+	}
+
+	fmt.Printf("Response from handshake: %v\n", hsh)
+
+	if hsh.PeerId != peerId {
+		return nil, errors.New("Peer ids does not match")
 	}
 
 	return c, nil
